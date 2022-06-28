320271ed4248928b3607354d13c909413b8bc345
LodyginArtyom <articrafter123@mail.ru>
2022-06-28 15:03:21 +0300

    start

---
+++ iFinance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iFinance
+//
+//  Created by Артем Лодыгин on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
